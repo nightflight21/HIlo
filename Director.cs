17d9ad3f123732de0243ec784ee849bb2f4f80a0
MDavidCole <mdcgr33n@gmail.com>
2022-05-11 11:03:49 -0600

    Defined the DetermineHiLo and Adjust Score functions

--- Director.cs
+++ Director.cs
@@ -10,5 +10,29 @@ namespace HiLo{
         
         // This is the variable that will store the current score of the user.
 
+        static bool DetermineHiLo(string guess, int currentCard, int newCard){
+            string answer = "c";
+            if (newCard > currentCard)
+            {answer = "h";}
+            else if (newCard < currentCard)
+            {answer = "l";}
+            else
+            {answer = guess;}
+
+            if (answer == guess)
+            {return true;}
+            else
+            {return false;}
+        }
+
+        static int AdjustScore(int score, bool correct)
+        {
+            if (correct == true)
+            {score = score + 100;}
+            else
+            {score = score - 75;}
+
+            return score;
+        }
     }
 }
