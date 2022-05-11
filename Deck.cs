a394f2251445b94e1971f71aa1d0b94084cfe09e
nightflight21 <104380966+nightflight21@users.noreply.github.com>
2022-05-11 09:12:11 -0700

    deck class, added constuctDeck and draw

--- Deck.cs
+++ Deck.cs
@@ -1,8 +1,20 @@
 
 namespace HiLo {
     class Deck {
-        public void deck() {
+        List<int> deck = new List<int>();
+        public void constuctDeck() {
+            for (int i = 0; i<4; i++) {
+                for (int j = 0; i<13; i++){
+                    deck.Add(j);
+                }
+            }
+        }
 
+        public int draw() {
+            var random = new Random();
+            int index = random.Next(this.deck.Count);
+            this.deck.Remove(index);
+            return this.deck[index];
         }
     }
 }
